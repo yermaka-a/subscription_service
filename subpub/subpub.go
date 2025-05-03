d7a23490a3d0d74996c82c4618edecc5d19bc826
yermaka <ermak.080@mail.ru>
2025-05-04 00:17:17 +0800

    rewrite subpub

--- subpub/subpub.go
+++ subpub/subpub.go
@@ -13,6 +13,7 @@ type Subscription interface {
 }
 
 type SubPub interface {
+	//subject - событие
 	Subscribe(subject string, cb MessageHandler) (Subscription, error)
 
 	Publish(subject string, msg interface{}) error
@@ -21,74 +22,96 @@ type SubPub interface {
 }
 
 func NewSubPub() SubPub {
-	return &eventBus{
-		handlers: make(map[string][]MessageHandler),
-	}
+	// return &eventBus{
+	// 	events: make(map[string][]MessageHandler, 0),
+	// }
+	panic("")
 }
 
 type subscription struct {
 	unsubscribe func()
 }
 
-func (s *subscription) Unsubscribe() {
-	s.unsubscribe()
+func (sb *subscription) Unsubscribe() {
+	sb.unsubscribe()
 }
 
 type eventBus struct {
-	handlers map[string][]MessageHandler
-	lock     sync.RWMutex
-	closed   bool
+	// хэш-таблица со списком хэндлеров для subject
+	events   map[string][]MessageHandler
+	mx       sync.RWMutex
+	isClosed bool
 }
 
 func (eb *eventBus) Subscribe(subject string, cb MessageHandler) (Subscription, error) {
-	eb.lock.Lock()
-	defer eb.lock.Unlock()
-
-	if eb.closed {
-		return nil,
-			errors.New("bus is cloed")
+	eb.mx.Lock()
+	defer eb.mx.Unlock()
+	if eb.isClosed {
+		return nil, errors.New("bus is closed")
 	}
-
-	eb.handlers[subject] = append(eb.handlers[subject], cb)
-
-	idx := len(eb.handlers[subject]) - 1
-
+	eb.events[subject] = append(eb.events[subject], cb)
+	idx := len(eb.events[subject]) - 1
 	return &subscription{
 		unsubscribe: func() {
-			eb.lock.Lock()
-			defer eb.lock.Unlock()
-			if eb.closed {
+			eb.mx.Lock()
+			defer eb.mx.Unlock()
+			if eb.isClosed {
 				return
 			}
-
-			handlers := eb.handlers[subject]
-			eb.handlers[subject] = append(handlers[:idx], handlers[idx+1:]...)
+			eb.events[subject] = append(eb.events[subject][:idx], eb.events[subject][idx+1:]...)
+			// Если список пуст, то удаляем ключ
+			if len(eb.events[subject]) == 0 {
+				delete(eb.events, subject)
+			}
 		},
 	}, nil
 }
 
 func (eb *eventBus) Publish(subject string, msg interface{}) error {
-	eb.lock.RLock()
-	defer eb.lock.RUnlock()
-	if eb.closed {
+	eb.mx.RLock()
+	defer eb.mx.RUnlock()
+	if eb.isClosed {
 		return errors.New("bus is closed")
 	}
-
-	if handlers, found := eb.handlers[subject]; found {
-		for _, handler := range handlers {
-			go handler(msg)
-		}
+	handlers, isExists := eb.events[subject]
+	if !isExists {
+		return errors.New("event isn't found")
 	}
+	copyHandlers := make([]MessageHandler, len(handlers))
+	copy(copyHandlers, handlers)
+	go func() {
+		for _, handler := range copyHandlers {
+			handler(msg)
+		}
+	}()
 	return nil
 }
 
 func (eb *eventBus) Close(ctx context.Context) error {
-	eb.lock.Lock()
-	defer eb.lock.Unlock()
-
-	if eb.closed {
+	eb.mx.Lock()
+	defer eb.mx.Unlock()
+	if eb.isClosed {
 		return errors.New("bus is already closed")
 	}
-	eb.closed = true
+	done := make(chan struct{})
+	select {
+	case <-ctx.Done():
+		eb.isClosed = true
+		eb.events = nil
+		return ctx.Err()
+	default:
+		go func() {
+			select {
+			case <-ctx.Done():
+				eb.mx.Lock()
+				defer eb.mx.Unlock()
+				eb.isClosed = true
+				eb.events = nil
+				close(done)
+			case <-done:
+				return
+			}
+		}()
+	}
 	return nil
 }
